bf997184f3642b83b818ce4e220f32ab6d21d90f
Cipiri <szymon789-95@wp.pl>
2023-10-26 22:06:46 +0200

    zmiany w login.vue, błąd multi-word-component-names

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,28 +1,29 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div>
-      <h1>Login</h1>
-      <form @submit="login">
-        <input v-model="email" type="text" placeholder="Email" />
-        <input v-model="password" type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        email: '',
-        password: ''
-      };
-    },
-    methods: {
-      login() {
-        // Tutaj możesz dodać logikę logowania, np. za pomocą API lub serwera
-        // Po poprawnym zalogowaniu możesz przekierować użytkownika w routerze
-        this.$router.push({ name: 'Catalog' });
-      }
+  <div>
+    <h1>Login</h1>
+    <form @submit="login">
+      <input v-model="email" type="text" placeholder="Email" />
+      <input v-model="password" type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      // Tutaj dodaj logikę logowania, np. wywołując API lub sprawdzając dane logowania
+      // Jeśli logowanie powiedzie się, przekieruj użytkownika na inną stronę
+      this.$router.push({ name: 'Catalog' });
     }
-  };
-  </script>
+  }
+};
+</script>
